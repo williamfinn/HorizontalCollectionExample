18fb299ef84d276e7081f93006b2c104ff9ef301
Liam Finn <william.finn@hotmail.com>
2020-08-31 12:42:02 +0100

    Initial Commit

---
+++ collectiontest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectiontest
+//
+//  Created by William Finn on 31/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
